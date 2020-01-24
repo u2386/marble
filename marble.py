d216e5cda54f76777426fad68698c93258f39a68
hugo <hugo.cavan2386@gmail.com>
2020-01-24 15:28:32 +0800

    feat: Scale

--- marble.py
+++ marble.py
@@ -5,11 +5,15 @@ from PIL import Image
 
 DEGREE = 16
 GRAY_COLORS = 'B0XCJVYOUL":!,. '[::-1]
-DEFAULT_IMG = 'Liberty.jpg'
+DEFAULT_IMG = 'David.jpg'
+
 
 def main():
     im_path = DEFAULT_IMG
     gim = Image.open(im_path).convert('L')
+    width, height = gim.size
+    scale = width // 64
+    gim.thumbnail((width//scale, height//scale))
     width, height = gim.size
     buffer = []
     pixels = gim.getdata()
